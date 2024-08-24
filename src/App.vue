4473718d50a744cb423290560fb60d55207da245
leelife96 <leelife77@naver.com>
2024-08-24 18:39:34 +0900

    ds

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <PageHeader />
+  // 헤더 컴포넌트
+  <router-view />
+  // 페이지 이동이 표시될 곳
+  <PageFooter />
+  // 푸터 컴포넌트
 </template>
+
+<script>
+// 헤더 페이지 경로
+import PageHeader from "@/components/PageHeader";
+
+// 푸터 페이지 경로
+import PageFooter from "@/components/PageFooter";
+
+export default {
+  name: "App",
+  components: {
+    PageFooter, // 푸터 페이지 컴포넌트 선언
+    PageHeader, // 헤더 페이지 컴포넌트 선언
+  },
+};
+</script>
 
 <style>
 #app {
