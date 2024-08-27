ef0ebb422eaee39fec15bf2ba199be5d29273344
leelife96 <leelife77@naver.com>
2024-08-27 22:11:37 +0900

    dsa

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
-  <PageHeader />
-  <router-link to="/user">UserList</router-link> |
-  <router-link to="/user/findById">UserSelect</router-link> |
-  <router-link to="/user/editById">UserUpdate</router-link> |
-  <router-link to="/user/save">UserCreate</router-link>
-  // 헤더 컴포넌트
-  <router-view />
-  // 페이지 이동이 표시될 곳
-  <PageFooter />
-  // 푸터 컴포넌트
+  <div id="app">
+    <PageHeader />
+    <!-- 헤더 컴포넌트 -->
+
+    <nav>
+      <router-link to="/user">UserList</router-link> |
+      <router-link to="/user/findById">UserSelect</router-link> |
+      <router-link to="/user/editById">UserUpdate</router-link> |
+      <router-link to="/user/save">UserCreate</router-link>
+    </nav>
+
+    <!-- 웹소켓 채팅 기능 -->
+    <div>
+      유저이름:
+      <input v-model="userName" type="text" />
+      내용: <input v-model="message" type="text" @keyup="sendMessage" />
+      <div v-for="(item, idx) in recvList" :key="idx">
+        <h3>유저이름: {{ item.userName }}</h3>
+        <h3>내용: {{ item.content }}</h3>
+      </div>
+    </div>
+
+    <router-view />
+    <!-- 페이지 이동이 표시될 곳 -->
+
+    <PageFooter />
+    <!-- 푸터 컴포넌트 -->
+  </div>
 </template>
 
 <script>
+import Stomp from "webstomp-client";
+import SockJS from "sockjs-client";
+
 // 헤더 페이지 경로
 import PageHeader from "@/components/PageHeader";
-
 // 푸터 페이지 경로
 import PageFooter from "@/components/PageFooter";
 
@@ -23,6 +43,61 @@ export default {
   components: {
     PageFooter, // 푸터 페이지 컴포넌트 선언
     PageHeader, // 헤더 페이지 컴포넌트 선언
+  },
+  data() {
+    return {
+      userName: "",
+      message: "",
+      recvList: [],
+    };
+  },
+  created() {
+    // App.vue가 생성되면 소켓 연결을 시도합니다.
+    this.connect();
+  },
+  methods: {
+    sendMessage(e) {
+      if (e.keyCode === 13 && this.userName !== "" && this.message !== "") {
+        this.send();
+        this.message = "";
+      }
+    },
+    send() {
+      console.log("Send message:" + this.message);
+      if (this.stompClient && this.stompClient.connected) {
+        const msg = {
+          userName: this.userName,
+          content: this.message,
+        };
+        this.stompClient.send("/receive", JSON.stringify(msg), {});
+      }
+    },
+    connect() {
+      const serverURL = "http://localhost:8787";
+      let socket = new SockJS(serverURL);
+      this.stompClient = Stomp.over(socket);
+      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`);
+      this.stompClient.connect(
+        {},
+        (frame) => {
+          // 소켓 연결 성공
+          this.connected = true;
+          console.log("소켓 연결 성공", frame);
+          // 서버의 메시지 전송 endpoint를 구독합니다.
+          this.stompClient.subscribe("/send", (res) => {
+            console.log("구독으로 받은 메시지 입니다.", res.body);
+
+            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+            this.recvList.push(JSON.parse(res.body));
+          });
+        },
+        (error) => {
+          // 소켓 연결 실패
+          console.log("소켓 연결 실패", error);
+          this.connected = false;
+        }
+      );
+    },
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
-const { defineConfig } = require("@vue/cli-service");
+/* const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "../src/main/resources/static", //Build Directory
-  /*   devServer: {
+  devServer: {
     proxy: "http://localhost:8787", // Spring Boot Server
-  }, */
-});
+  },
+}); */
 
-/* const target = "http://localhost:8787";
+const target = "http://localhost:8787";
 module.exports = {
   devServer: {
     port: 8787,
@@ -19,4 +19,3 @@ module.exports = {
     },
   },
 };
- */
