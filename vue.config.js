0451845cf393eb617a84bb3cc6c76d5f45bea888
leelife96 <leelife77@naver.com>
2024-09-08 19:50:24 +0900

    09-08commit

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,35 @@ module.exports = defineConfig({
   },
 }); */
 
+/* module.exports = {
+  devServer: {
+    port: 9000, // Vue.js를 9000에서 실행
+    proxy: {
+      "/api": {
+        target: "http://localhost:8787", // Spring Boot 서버
+        changeOrigin: true,
+        pathRewrite: { "^/api": "" }, // 경로에서 /api 제거
+      },
+    },
+  },
+};
+ */
+
+/* const target = "http://3.34.231.185:8787";
+module.exports = {
+  devServer: {
+    port: 9000, // Vue.js를 9000에서 실행
+    proxy: {
+      "/api": {
+        target: target, // Spring Boot 서버
+        changeOrigin: true,
+        pathRewrite: { "^/api": "" }, // 경로에서 /api 제거
+      },
+    },
+  },
+};
+ */
+
 const target = "http://localhost:8787";
 module.exports = {
   devServer: {
